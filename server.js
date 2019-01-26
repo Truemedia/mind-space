f4a426417297105f936a485048d2f36b5ac30a46
Wade Penistone <wadespackages@yahoo.com>
2019-01-26 12:38:16 +0000

    Simple server

---
+++ server.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const http = require('http')
+
+fs.readFile('./src/index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(3000);
+});
